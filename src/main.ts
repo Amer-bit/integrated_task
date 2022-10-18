0bdbda684e67ad22f2ba026ab8fc17e34bb4fd8a
amer-bit <ny.1995@outlook.com>
2022-10-18 18:15:07 +0300

    initial app scaffold

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.use(compression());
+  app.enableCors();
+  app.use(helmet());
+  app.use(csurf());
+
+
+  await app.listen(3000);
+}
+bootstrap();
